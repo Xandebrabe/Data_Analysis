758561a89c3c82d878c92a89acc8674066091c09
Nícolas <nicolasdamotaarruda@hotmail.com>
2021-12-20 19:16:21 -0300

    Criando git ignore e escondendo coisas

--- Data_Analysis/settings.py
+++ Data_Analysis/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-re9k3mkixf1#*d@5im$-#3ws&e7(d5$x9p1(j&b7@rat0f!ng2'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
