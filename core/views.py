64f5fc7e22340a9a8366292f4dd313296155d8e3
Nícolas <nicolasdamotaarruda@hotmail.com>
2022-01-08 19:45:44 -0300

    Alterando menu e página de contato

--- core/views.py
+++ core/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 import pandas as pd
 
-url_dados = 'https://github.com/CSSEGISandData/COVID-19/blob/6069101a460264889fbab70daffba3dcbe24ed00/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv?raw=true'
+#url_dados = 'https://github.com/CSSEGISandData/COVID-19/blob/6069101a460264889fbab70daffba3dcbe24ed00/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv?raw=true'
 
-dados = pd.read_csv(url_dados+'.csv')
+url_dados = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+
+dados_df = pd.read_csv(url_dados)
 
 def index(request):
-    #print (dados[1])
+    #print(dados_df.iloc[ : , 4:dados_df.shape[1]])
+    print(dados_df[['Country/Region', '1/7/22']])
     info = {
-        'dados':dados} 
-    return render(request, 'index.html',info)
+        'dados':dados_df} 
+    return render(request, 'index.html', info)
 
 
 def sobrenos(request):
