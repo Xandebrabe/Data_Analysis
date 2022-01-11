c15df50cff66e91a3296e1617fa6b8577743f69f
Nícolas <nicolasdamotaarruda@hotmail.com>
2022-01-11 02:45:01 -0300

    Fazendo o makemigrations para a tabela de paises

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Paises(models.Model):
+    nome_ingles: models.CharField("nome_ingles", max_length=50)
+    nome_portugues: models.CharField("nome_portugues", max_length=50)
+    bandeira_url: models.CharField("bandeira_url", max_length=40)
