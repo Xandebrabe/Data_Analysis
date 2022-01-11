9a76f9db4a1a5b8e7de379eca680119d7ad930fc
Nícolas <nicolasdamotaarruda@hotmail.com>
2022-01-11 03:09:48 -0300

    Criando outro model para países

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Paises(models.Model):
-    nome_ingles: models.CharField("nome_ingles", max_length=50)
-    nome_portugues: models.CharField("nome_portugues", max_length=50)
-    bandeira_url: models.CharField("bandeira_url", max_length=40)
+    nome_ingles = models.CharField("nome_ingles", max_length=50)
+    nome_portugues = models.CharField("nome_portugues", max_length=50)
+    bandeira_url = models.CharField("bandeira_url", max_length=40)
